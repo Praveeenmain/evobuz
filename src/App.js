c8b134efbec3e62dbcf47acbfe77d5eaf8398b1b
Praveeenmain <praveenlapacc@gmail.com>
2024-07-18 13:13:45 +0530

    added all pages with react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
+import ProfilePage from './Components/ProfilePage'
 import Contact from './Components/Contact'
-
+import ServicePage from './Components/servicePage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
     
-      <Contact/>
+    <BrowserRouter>
+        <Routes>
+          <Route index element={<Contact />} />
+          <Route path="/profile" element={<ProfilePage/>} />
+          <Route path="/service" element={<ServicePage/>} />
+         
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
