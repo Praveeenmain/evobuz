e274c0c57ebb2540460028457fb986f40f601a06
Praveeenmain <praveenlapacc@gmail.com>
2024-07-18 19:00:59 +0530

    added about section and footer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import ProfilePage from './Components/ProfilePage'
 import Contact from './Components/Contact'
 import ServicePage from './Components/servicePage'
+import About from './Components/About';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+const Home=()=>{
+  return(
+    <h1> Hello ,Home</h1>
+  )
+}
 function App() {
   return (
     <div className="App">
     
     <BrowserRouter>
         <Routes>
-          <Route index element={<Contact />} />
+          <Route index element={<Home/>} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/profile" element={<ProfilePage/>} />
           <Route path="/service" element={<ServicePage/>} />
-         
+          <Route path="/About" element={<About/>} />
+          
           
         </Routes>
       </BrowserRouter>
