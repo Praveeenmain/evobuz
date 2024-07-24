2cb88c512ba5cd6eb2c1d641877ec596aec29db2
Praveeenmain <praveenlapacc@gmail.com>
2024-07-24 11:49:29 +0530

    added notification page and notification card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Contact from './Components/Contact'
 import ServicePage from './Components/servicePage'
 import About from './Components/About';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotificationPage from './Components/NotificationPage'
 const Home=()=>{
   return(
     <h1> Hello ,Home</h1>
@@ -19,6 +20,7 @@ function App() {
           <Route path="/profile" element={<ProfilePage/>} />
           <Route path="/service" element={<ServicePage/>} />
           <Route path="/About" element={<About/>} />
+          <Route path="/Notification" element={<NotificationPage/>} />
           
           
         </Routes>
